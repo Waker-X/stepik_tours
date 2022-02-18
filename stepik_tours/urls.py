925d7ad40d28599c431f211c1db06da144f80e1d
Viktor Baranov <baranov71viktor@gmail.com>
2022-02-18 21:52:07 +0300

    Добавлены обработчики HTTP кодов

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from tours.views import main_view, departure_view, tour_view
+from tours.views import main_view, departure_view, tour_view, custom_handler400, custom_handler403, custom_handler404, \
+    custom_handler500
 
+handler400 = custom_handler400
+handler403 = custom_handler403
+handler404 = custom_handler404
+handler500 = custom_handler500
 
 urlpatterns = {
     path('', main_view),
--- tours/views.py
+++ tours/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseBadRequest, HttpResponseNotFound, HttpResponseForbidden, HttpResponseServerError
 
 
 def main_view(request):
@@ -11,3 +12,19 @@ def departure_view(request, departure):
 
 def tour_view(request, id):
     return render(request, 'tour/tour.html')
+
+
+def custom_handler400(request, exception):
+    return HttpResponseBadRequest('Неверный запрос!')
+
+
+def custom_handler403(request, exception):
+    return HttpResponseForbidden('Доступ запрещен!')
+
+
+def custom_handler404(request, exception):
+    return HttpResponseNotFound('Страница не найдена!')
+
+
+def custom_handler500(request):
+    return HttpResponseServerError('Ошибка сервера!')
